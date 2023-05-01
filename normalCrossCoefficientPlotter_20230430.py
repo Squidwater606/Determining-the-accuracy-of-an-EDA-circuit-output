ddde19b3686db63ec725640de2ba27fa9b357308
Rudy <125066362+Squidwater606@users.noreply.github.com>
2023-05-01 03:40:08 +0100

    Add files via upload
    Final-final-final-update 2.0: 
    Code now determines phase-shift of EDA output signal using cross-correlation.

--- normalCrossCoefficientPlotter_20230430.py
+++ normalCrossCoefficientPlotter_20230430.py
@@ -37,28 +37,34 @@ def timeStep(time):
     return np.average(np.diff(time))
 
 def fft(func,time): 
-    shape = time.shape[0]
+    shape = len(time)
     if np.any(np.isnan(func)) or np.any(np.isinf(func)):
         func[np.isnan(func) | np.isinf(func)] = 1e-15
-    return np.fft.rfft(func,n=shape),np.fft.rfftfreq(time.shape[0],timeStep(time))
+    Ft,freqs = np.fft.rfft(func,n=shape),np.fft.rfftfreq(len(time),timeStep(time))
+    return Ft,freqs
 
 def ifft(ft,shape):
     if np.any(np.isnan(ft)) or np.any(np.isinf(ft)):
         ft[np.isnan(ft) | np.isinf(ft)] = 1e-15
-    return np.fft.irfft(ft,n=shape)
+    iFt = np.fft.irfft(ft,n=shape)
+    return iFt
 
-def crossCorrelate(func1,func2,time):
+def crossCorr(func1,func2,time):
     func1Fft,func2Fft = fft(func1,time)[0],fft(func2,time)[0]
-    ftConvolve = np.conj(func1Fft)*func2Fft
-    return ifft(ftConvolve,time.shape[0])
+    ftConvolve1 = np.conj(func1Fft)*func2Fft
+    ftConvolve2 = np.conj(func2Fft)*func1Fft
+    convolve1 = ifft(ftConvolve1,len(time))
+    convolve2 = ifft(ftConvolve2,len(time))
+    return convolve1,convolve2
 
-def normalizedCrossCorrelation(func1,func2,time):
-    C12 = crossCorrelate(func1,func2,time)
-    C21 = crossCorrelate(func2,func1,time)
+def normCrossCorr(func1,func2,time):
+    C12,C21 = crossCorr(func1,func2,time)
     normFactor = np.sqrt(np.sum(func1**2)*np.sum(func2**2))
     normC12 = C12 / normFactor
     normC21 = C21 / normFactor
-    return normC12,normC21,(np.max(normC12)+np.max(normC21))/2
+    nccCoeff = (np.max(np.abs(normC12))+np.max(np.abs(normC21)))/2
+    phaseDiff = -np.arctan2(np.sum(normC21 * normC12), np.sum(normC21**2))
+    return normC12,normC21,nccCoeff,phaseDiff
 
 def solveODEs(func,time,γ,ω):
     try:
@@ -79,7 +85,7 @@ def solveODEs(func,time,γ,ω):
         print(f"An error occurred in solveODE: {e}")
         return None
 
-def plot(filepaths,columnIndex,γ,ω,scalings):
+def plotFull(filepaths,columnIndex,γ,ω,scalings):
     fig, axs = plt.subplots(nrows=5, ncols=len(filepaths), figsize=(20, 10))
     for i, filepath in enumerate(filepaths):
         data = readData(filepath)
@@ -88,7 +94,7 @@ def plot(filepaths,columnIndex,γ,ω,scalings):
             timeZeroed = time - time[0]
             solvedODE = solveODEs(inputSignal, time, γ, ω)
             y,yPrime,yDoublePrime = solvedODE[0][::-1],solvedODE[1][::-1],solvedODE[2][::-1]
-            normC12,normC21,crossCorrCoef = normalizedCrossCorrelation(integAmp2Signal*-1,y[::-1],time)
+            normC12,normC21,crossCorrCoef,phaseDiff = normCrossCorr(integAmp2Signal*-1,y[::-1],time)
             axs[0][i].plot(timeZeroed, inputSignal,color='c')
             axs[1][i].plot(timeZeroed,y,color='r')
             axs[2][i].plot(timeZeroed,yPrime,color='r')
@@ -98,17 +104,43 @@ def plot(filepaths,columnIndex,γ,ω,scalings):
             axs[3][i].plot(timeZeroed,sumAmpSignal*scalings[2],color='g')
             axs[4][i].plot(timeZeroed,normC12,color='m')
             axs[4][i].plot(timeZeroed,normC21,color='y')
-            axs[4][i].text(0.95, 0.95, f"Normalized 'r': {crossCorrCoef:.2f}", transform=axs[4][i].transAxes, ha='right', va='top', bbox=dict(facecolor='white', alpha=0.5))
+            axs[1][i].text(0.95, 0.95, f"Phase-Difference: {phaseDiff:.2f}", transform=axs[1][i].transAxes, ha='right', va='top', bbox=dict(facecolor='white', alpha=0.5))
+            axs[4][i].text(0.95, 0.95, f"NCC Coefficient 'r': {crossCorrCoef:.2f}", transform=axs[4][i].transAxes, ha='right', va='top', bbox=dict(facecolor='white', alpha=0.5))
+    fig.text(0.5, 0.04, 'Time (s)', ha='center')
+    fig.text(0.04, 0.5, 'Voltage (V)', va='center', rotation='vertical')
+    plt.subplots_adjust(wspace=0.4, hspace=0.4)
+    plt.show()
+
+def plotShrunk(filepaths,columnIndex,γ,ω,scalings):
+    fig, axs = plt.subplots(nrows=3, ncols=len(filepaths), figsize=(20, 10))
+    for i, filepath in enumerate(filepaths):
+        data = readData(filepath)
+        if data is not None:
+            time,inputSignal,integAmp2Signal = data[columnIndex[0]],data[columnIndex[1]],data[columnIndex[4]]
+            timeZeroed = time - time[0]
+            solvedODE = solveODEs(inputSignal, time, γ, ω)
+            y = solvedODE[0][::-1]
+            normC12,normC21,crossCorrCoef,phaseDiff = normCrossCorr(integAmp2Signal*-1,y[::-1],time)
+            axs[0][i].plot(timeZeroed, inputSignal,color='c')
+            axs[1][i].plot(timeZeroed,y,color='r')
+            axs[1][i].plot(timeZeroed,integAmp2Signal*scalings[0],color='g')
+            axs[2][i].plot(timeZeroed,normC12,color='m')
+            axs[2][i].plot(timeZeroed,normC21,color='y')
+            axs[1][i].text(0.95, 0.95, f"Phase-Difference: {phaseDiff:.2f}", transform=axs[1][i].transAxes, ha='right', va='top', bbox=dict(facecolor='white', alpha=0.5))
+            axs[2][i].text(0.95, 0.95, f"NCC Coefficient 'r': {crossCorrCoef:.2f}", transform=axs[2][i].transAxes, ha='right', va='top', bbox=dict(facecolor='white', alpha=0.5))
     fig.text(0.5, 0.04, 'Time (s)', ha='center')
     fig.text(0.04, 0.5, 'Voltage (V)', va='center', rotation='vertical')
     plt.subplots_adjust(wspace=0.4, hspace=0.4)
     plt.show()
 
 # Critical-Damping:
-plot(criticalDampingFilepaths,columnIndex,cdγ,cdω,scalings)
+plotFull(criticalDampingFilepaths,columnIndex,cdγ,cdω,scalings)
+plotShrunk(criticalDampingFilepaths,columnIndex,cdγ,cdω,scalings)
 
 # Overdamping:
-plot(overDampingFilepaths,columnIndex,odγ,odω,scalings)
+plotFull(overDampingFilepaths,columnIndex,odγ,odω,scalings)
+plotShrunk(overDampingFilepaths,columnIndex,odγ,odω,scalings)
 
 # Underdamping:
-plot(underDampingFilepaths,columnIndex,udγ,udω,scalings)
+plotFull(underDampingFilepaths,columnIndex,udγ,udω,scalings)
+plotShrunk(underDampingFilepaths,columnIndex,udγ,udω,scalings)
