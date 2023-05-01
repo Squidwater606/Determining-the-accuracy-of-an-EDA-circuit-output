619ea0110de9eeb93837cf36dfd1dae9ab1489dd
Rudy <125066362+Squidwater606@users.noreply.github.com>
2023-05-01 04:39:48 +0100

    Add files via upload
    Final-final-final-final-final update:
    fixed CRITICAL ERRORS

--- normalCrossCoefficientPlotter_20230430.py
+++ normalCrossCoefficientPlotter_20230430.py
@@ -18,6 +18,8 @@ udγ,udω = 0.5,0.5
 columnIndex = ['in s','C1 in V','C2 in V','C3 in V','C4 in V']
 scalings = [-1,-10,-100]
 
+# note that ω represents the square natural frequency
+
 # define functions:
 def readData(path):
     try:
@@ -40,8 +42,8 @@ def fft(func,time):
     shape = len(time)
     if np.any(np.isnan(func)) or np.any(np.isinf(func)):
         func[np.isnan(func) | np.isinf(func)] = 1e-15
-    Ft,freqs = np.fft.rfft(func,n=shape),np.fft.rfftfreq(len(time),timeStep(time))
-    return Ft,freqs
+    ft,freqs = np.fft.rfft(func,n=shape),np.fft.rfftfreq(len(time),timeStep(time))
+    return ft,freqs
 
 def ifft(ft,shape):
     if np.any(np.isnan(ft)) or np.any(np.isinf(ft)):
@@ -49,52 +51,44 @@ def ifft(ft,shape):
     iFt = np.fft.irfft(ft,n=shape)
     return iFt
 
-def crossCorr(func1,func2,time):
-    func1Fft,func2Fft = fft(func1,time)[0],fft(func2,time)[0]
-    ftConvolve1 = np.conj(func1Fft)*func2Fft
-    ftConvolve2 = np.conj(func2Fft)*func1Fft
+def crossCorr(ft1,ft2,time):
+    ftConvolve1 = np.conj(ft1)*ft2
+    ftConvolve2 = np.conj(ft2)*ft1
     convolve1 = ifft(ftConvolve1,len(time))
     convolve2 = ifft(ftConvolve2,len(time))
     return convolve1,convolve2
 
-def normCrossCorr(func1,func2,time):
-    C12,C21 = crossCorr(func1,func2,time)
+def normCrossCorr(ft1,ft2,func1,func2,time):
+    C12,C21 = crossCorr(ft1,ft2,time)
     normFactor = np.sqrt(np.sum(func1**2)*np.sum(func2**2))
-    normC12 = C12 / normFactor
-    normC21 = C21 / normFactor
+    normC12 = C12/normFactor
+    normC21 = C21/normFactor
     nccCoeff = (np.max(np.abs(normC12))+np.max(np.abs(normC21)))/2
-    phaseDiff = -np.arctan2(np.sum(normC21 * normC12), np.sum(normC21**2))
+    phaseDiff = np.arccos(np.sum(normC21*normC12)/(np.sqrt(np.sum(normC21**2))*np.sqrt(np.sum(normC12**2))))
+    if phaseDiff < 0:
+        phaseDiff += np.pi
     return normC12,normC21,nccCoeff,phaseDiff
 
-def solveODEs(func,time,γ,ω):
-    try:
-        ftFunc,freqs = fft(func,time)
-        shape = time.shape[0]
-        yFft = ftFunc/(-(freqs**2)-(1j)*γ*(freqs)+ω)
-        y = ifft(yFft,shape)
-        yPrime = np.gradient(y,time)
-        yDoublePrime = np.gradient(yPrime,time)
-        return y,yPrime,yDoublePrime
-    except AttributeError:
-        print("Invalid argument passed to solveODE")
-        return None
-    except ValueError:
-        print("Invalid input value for solveODE")
-        return None
-    except Exception as e:
-        print(f"An error occurred in solveODE: {e}")
-        return None
+def solve(sigInpt,func,time,γ,ω):
+    shape = len(time)
+    ftInpt,freqs = fft(sigInpt,time)
+    ftFunc,f = fft(func,time)
+    yFft = ftInpt/(-(freqs**2)+γ*(1j)*freqs+ω)
+    y = ifft(yFft,shape)
+    yPrime = np.gradient(y, time)
+    yDoublePrime = np.gradient(yPrime, time)
+    normC12,normC21,nccCoeff,phaseDiff = normCrossCorr(ftFunc,yFft,func,y,time)
+    return y,yPrime,yDoublePrime,normC12,normC21,nccCoeff,phaseDiff
 
 def plotFull(filepaths,columnIndex,γ,ω,scalings):
     fig, axs = plt.subplots(nrows=5, ncols=len(filepaths), figsize=(20, 10))
     for i, filepath in enumerate(filepaths):
         data = readData(filepath)
         if data is not None:
-            time,inputSignal,sumAmpSignal,invAmpSignal,integAmp2Signal  = data[columnIndex[0]],data[columnIndex[1]],data[columnIndex[2]],data[columnIndex[3]],data[columnIndex[4]]
+            time,inputSignal,sumAmpSignal,invAmpSignal,integAmp2Signal = data[columnIndex[0]],data[columnIndex[1]],data[columnIndex[2]],data[columnIndex[3]],data[columnIndex[4]]
             timeZeroed = time - time[0]
-            solvedODE = solveODEs(inputSignal, time, γ, ω)
-            y,yPrime,yDoublePrime = solvedODE[0][::-1],solvedODE[1][::-1],solvedODE[2][::-1]
-            normC12,normC21,crossCorrCoef,phaseDiff = normCrossCorr(integAmp2Signal*-1,y[::-1],time)
+            solved = solve(inputSignal,integAmp2Signal,time,γ,ω)
+            y,yPrime,yDoublePrime,normC12,normC21,nccCoeff,phaseDiff = solved[0],solved[1],solved[2],solved[3],solved[4],solved[5],solved[6]
             axs[0][i].plot(timeZeroed, inputSignal,color='c')
             axs[1][i].plot(timeZeroed,y,color='r')
             axs[2][i].plot(timeZeroed,yPrime,color='r')
@@ -105,7 +99,7 @@ def plotFull(filepaths,columnIndex,γ,ω,scalings):
             axs[4][i].plot(timeZeroed,normC12,color='m')
             axs[4][i].plot(timeZeroed,normC21,color='y')
             axs[1][i].text(0.95, 0.95, f"Phase-Difference: {phaseDiff:.2f}", transform=axs[1][i].transAxes, ha='right', va='top', bbox=dict(facecolor='white', alpha=0.5))
-            axs[4][i].text(0.95, 0.95, f"NCC Coefficient 'r': {crossCorrCoef:.2f}", transform=axs[4][i].transAxes, ha='right', va='top', bbox=dict(facecolor='white', alpha=0.5))
+            axs[4][i].text(0.95, 0.95, f"NCC Coefficient 'r': {nccCoeff:.2f}", transform=axs[4][i].transAxes, ha='right', va='top', bbox=dict(facecolor='white', alpha=0.5))
     fig.text(0.5, 0.04, 'Time (s)', ha='center')
     fig.text(0.04, 0.5, 'Voltage (V)', va='center', rotation='vertical')
     plt.subplots_adjust(wspace=0.4, hspace=0.4)
@@ -118,16 +112,15 @@ def plotShrunk(filepaths,columnIndex,γ,ω,scalings):
         if data is not None:
             time,inputSignal,integAmp2Signal = data[columnIndex[0]],data[columnIndex[1]],data[columnIndex[4]]
             timeZeroed = time - time[0]
-            solvedODE = solveODEs(inputSignal, time, γ, ω)
-            y = solvedODE[0][::-1]
-            normC12,normC21,crossCorrCoef,phaseDiff = normCrossCorr(integAmp2Signal*-1,y[::-1],time)
+            solved = solve(inputSignal,integAmp2Signal,time,γ,ω)
+            y,normC12,normC21,nccCoeff,phaseDiff = solved[0],solved[3],solved[4],solved[5],solved[6]
             axs[0][i].plot(timeZeroed, inputSignal,color='c')
             axs[1][i].plot(timeZeroed,y,color='r')
             axs[1][i].plot(timeZeroed,integAmp2Signal*scalings[0],color='g')
             axs[2][i].plot(timeZeroed,normC12,color='m')
             axs[2][i].plot(timeZeroed,normC21,color='y')
             axs[1][i].text(0.95, 0.95, f"Phase-Difference: {phaseDiff:.2f}", transform=axs[1][i].transAxes, ha='right', va='top', bbox=dict(facecolor='white', alpha=0.5))
-            axs[2][i].text(0.95, 0.95, f"NCC Coefficient 'r': {crossCorrCoef:.2f}", transform=axs[2][i].transAxes, ha='right', va='top', bbox=dict(facecolor='white', alpha=0.5))
+            axs[2][i].text(0.95, 0.95, f"NCC Coefficient 'r': {nccCoeff:.2f}", transform=axs[2][i].transAxes, ha='right', va='top', bbox=dict(facecolor='white', alpha=0.5))
     fig.text(0.5, 0.04, 'Time (s)', ha='center')
     fig.text(0.04, 0.5, 'Voltage (V)', va='center', rotation='vertical')
     plt.subplots_adjust(wspace=0.4, hspace=0.4)
